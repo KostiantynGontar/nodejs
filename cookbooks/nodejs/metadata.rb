4ed62052362f9a664d2ec2f8e85715b105516bea
Konstantin Gontar <gontarkonstantin@gmail.com>
2015-12-25 11:52:24 +0200

    Initial commit

---
+++ cookbooks/nodejs/metadata.rb
@@ -0,0 +1,7 @@
+name 'nodejs'
+maintainer 'Gontar Konstantin'
+maintainer_email 'gontarkonstantin@gmail.com'
+license 'all_rights'
+description 'Installs/Configures nodejs'
+long_description 'Installs/Configures nodejs'
+version '0.1.0'
---
+++ cookbooks/nodejs/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: nodejs
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
